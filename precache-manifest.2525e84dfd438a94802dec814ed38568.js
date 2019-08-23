0d11d9df57b9d3ae343086097194ca020dae79d0
ahmed.7ussien <a.7ussien2013@gmail.com>
2019-08-23 02:21:01 +0200

    Updates

---
+++ precache-manifest.2525e84dfd438a94802dec814ed38568.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0db248c800fe5a62d588a0bccf3e8288",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "b66c5029835a5c6ffb79",
+    "url": "/movie-app/static/css/main.5a79728a.chunk.css"
+  },
+  {
+    "revision": "a094e1eabd0b5addd94b",
+    "url": "/movie-app/static/js/2.fc8fc673.chunk.js"
+  },
+  {
+    "revision": "b66c5029835a5c6ffb79",
+    "url": "/movie-app/static/js/main.c87c28b6.chunk.js"
+  },
+  {
+    "revision": "d8c18da1c3f645f35d82",
+    "url": "/movie-app/static/js/runtime~main.1593d493.js"
+  }
+]);
